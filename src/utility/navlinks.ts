9e51997fac5d32f3656e936259e8ef476ddb9303
Jasmin Hidanovic <dexinho008@gmail.com>
2024-03-29 23:32:51 +0100

    navlinks

---
+++ src/utility/navlinks.ts
@@ -0,0 +1,17 @@
+import { NavlinkProps } from "../../types/NavlinkProps";
+import { Navlink } from "../../types/NavlinkProps";
+
+export const navlinks: Navlink[] = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+];
