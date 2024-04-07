d90889728aecd7bc10dd21b1e822c86d969d96c1
Jasmin Hidanovic <dexinho008@gmail.com>
2024-04-07 23:54:19 +0200

    removed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Next.js 14",
@@ -17,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className='container'>
         <Navbar />
         {children}
       </body>
